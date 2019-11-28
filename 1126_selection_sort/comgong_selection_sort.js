fcb1b117c0bd249baf3c86415b3e0a3f2ba6c27b
noder7 <53991808+noder7@users.noreply.github.com>
2019-11-28 14:00:36 +0900

    update selection algorithm.

--- 1126_selection_sort/comgong_selection_sort.js
+++ 1126_selection_sort/comgong_selection_sort.js
@@ -1,16 +1,20 @@
 // Selection sort
-array = [ 5, 4, 3, 2, 1 ];
-let minIndex = 0;
+let array = [ 5, 4, 1, 2, 3 ];
+let minIndex;
 let temp = 0;
 
-for (let i = 1; i < array.length; i++) {
-    for (let j = i; j < array.length; j++) {
-        if (array[minIndex] > array[j]) {
-            temp = array[minIndex];
-            array[minIndex] = array[j];
-            array[j] = temp;
-        }
-    }
+for (let i = 0; i < array.length; i++) {
+
     minIndex = i;
+
+    for (let j = i+1; j < array.length; j++){
+        if(array[minIndex] > array[j])
+            minIndex = j;
+    }
+
+    temp = array[minIndex];
+    array[minIndex] = array[i];
+    array[i] = temp;
 }
+
 console.log(array);
